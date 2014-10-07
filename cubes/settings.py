cf83f46d3c1fbddbf546b776bc978859d56524e1
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2014-10-06 23:57:47 -0400

    adding setting for the site

--- cubes/settings.py
+++ cubes/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+if os.environ.get('PRODUCTION') == 'True':
+    DEBUG=False
+else:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -19,10 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&o^a@ni#y@2@(-j821t9nfs#hwom169y*4^edjno79(m6q8(ku'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
