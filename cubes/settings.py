e7395a6c7c903cb89f6b84e112f41126aab6954b
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2014-10-06 23:53:29 -0400

    adding files

--- cubes/settings.py
+++ cubes/settings.py
@@ -93,5 +93,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
